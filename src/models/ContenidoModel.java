aab73fd51dce0fe7ab08775621759f1748c28226
Alejandro Naranjo Núñez <naranjowow96@hotmail.com>
2017-10-15 21:32:29 +0200

    Added insert musica to db

---
+++ src/models/ContenidoModel.java
@@ -0,0 +1,35 @@
+package models;
+
+import dataStructures.Contenido;
+import dataStructures.Musica;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+class ContenidoModel {
+    ConnectDB con;
+
+    ContenidoModel() throws SQLException, ClassNotFoundException {
+        con = new ConnectDB();
+    }
+
+    int insertContenido(Contenido contenido) throws SQLException {
+        String sql = "INSERT INTO con_contenido(con_titulo, con_codigo, con_imagen, con_fecha_creacion)" +
+                "VALUES(?,?,?,now())";
+
+        PreparedStatement insertContenido = con.getConn().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+        insertContenido.setString(1, contenido.getTitulo());
+        insertContenido.setString(2, contenido.getCodigo());
+        insertContenido.setString(3, contenido.getImagen());
+        insertContenido.executeUpdate();
+
+        ResultSet rs = insertContenido.getGeneratedKeys();
+
+        rs.next();
+
+        return rs.getInt(1);
+    }
+
+}
