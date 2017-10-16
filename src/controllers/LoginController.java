105073fc1555488d40967570fefa9a98588502b9
Alejandro Naranjo Núñez <naranjowow96@hotmail.com>
2017-10-16 23:02:32 +0200

    added log calls

--- src/controllers/LoginController.java
+++ src/controllers/LoginController.java
@@ -2,6 +2,7 @@ package controllers;
 
 import dataStructures.User;
 import helpers.Logger;
+import helpers.TipoLog;
 import models.LoginModel;
 import views.VistaLogin;
 import views.VistaPrincipal;
@@ -12,13 +13,17 @@ import java.sql.SQLException;
 public class LoginController {
 
     public void login(String username, String password){
+        Logger.log("User login("+username+")");
+
         try {
             LoginModel loginModel = new LoginModel();
             User user = loginModel.getUser(username, password);
 
             if(user == null){
+                Logger.log("Login failed("+username+")");
                 //TODO: Print failed login to view
             } else {
+                Logger.log("Login correct("+username+")");
                 UserController.pk = user.getPk();
                 UserController.userName = user.getUserName();
 
@@ -28,6 +33,7 @@ public class LoginController {
             }
         } catch (SQLException | ClassNotFoundException e) {
             //TODO: Handle exception (Print error to view)
+            Logger.log("Exception in login", TipoLog.ERROR);
             Logger.log(e);
         }
     }
