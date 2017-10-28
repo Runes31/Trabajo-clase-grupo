16aa7c8d6db9ea79afd2e96349a7cbe0c6485b9b
Alejandro Naranjo Núñez <naranjowow96@hotmail.com>
2017-10-28 16:38:49 +0200

    comentario cap helper

--- src/helpers/CapituloHelper.java
+++ src/helpers/CapituloHelper.java
@@ -7,6 +7,12 @@ import java.io.File;
 import java.io.IOException;
 
 public class CapituloHelper {
+
+    /**
+     * Abre el archivo que guarda el capitulo de muestra con el programa por defecto
+     * @param libro
+     * @throws IOException
+     */
     public static void verCapituloMuestra(Libro libro) throws IOException {
         Desktop.getDesktop().open(getCapituloMuestra(libro));
     }
