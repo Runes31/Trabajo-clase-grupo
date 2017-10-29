76bf76085cf1cb16719a896a730a5d343715a085
Alejandro Naranjo Núñez <naranjowow96@hotmail.com>
2017-10-29 17:59:12 +0100

    Añadidas vistas de crear/editar

---
+++ src/views/VistaCrearPelicula.java
@@ -0,0 +1,13 @@
+package views;
+
+import dataStructures.Contenido;
+
+public class VistaCrearPelicula extends VistaCrear {
+    public VistaCrearPelicula(Contenido contenido) {
+        super(contenido);
+    }
+
+    public VistaCrearPelicula(){
+        super();
+    }
+}
